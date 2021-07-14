179f553a1d81c6595cf833a381ad301699dd9004
felipehenrique159 <felipecod6@hotmail.com>
2021-07-14 16:15:36 -0300

    Front end pagina Home/cadastro

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
-import api from '../../services/api'
+// import api from '../../services/api'
+import './home.css'
 
 export default function index() {
     return (
-        <h1>Home</h1>
+        <div className="container">
+            <div className="form-container">
+                <div className="banner-area">
+                    <label>Sistema Indicação</label>
+                </div>
+                <form >
+                    <label>Nome</label>
+                    <input type="text" />
+                    <label>Cpf</label>
+                    <input type="text" />
+                    <label>Telefone</label>
+                    <input type="text" />
+                    <label>Email</label>
+                    <input type="email" />
+                </form>
+
+                <button className="btn-form">Indicar</button>
+            </div>
+        </div>
+
+
+
+
     )
 }
